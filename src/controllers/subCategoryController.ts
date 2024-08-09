e131fd44501e60a9c8dc0d1679b101fae66f3c27
Derrick <pshubomi@gmail.com>
2024-08-09 17:35:45 +0100

    Added update functionality for subCategory

--- src/controllers/subCategoryController.ts
+++ src/controllers/subCategoryController.ts
@@ -25,4 +25,42 @@ const createSubCategory = async (req: Request, res: Response) => {
     });
   }
 };
-export { createSubCategory };
+
+// Update subCategory
+const updateSubCategory = async (req: Request, res: Response) => {
+  const { subCategoryId } = req.params;
+  const { name, description, image, status } = req.body;
+  try {
+    const subCategory = await SubCategory.findOne({ _id: subCategoryId });
+
+    // if (!name || !description || !image || !status) {
+    //     return res.status()
+    // }
+
+    if (!subCategory) {
+      return res.status(200).json({
+        error: true,
+        message: "subCategory not found, please try again with the correct ID",
+      });
+    }
+
+    if (name) subCategory.name = name;
+    if (description) subCategory.description = description;
+    if (image) subCategory.image = image;
+    if (status) subCategory.status = status;
+
+    return res.status(200).json({
+      error: false,
+      subCategory,
+      message: "SubCategory updated successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: true,
+      err,
+      messaage: "Internal server error, Please try again",
+    });
+  }
+};
+
+export { createSubCategory, updateSubCategory };
