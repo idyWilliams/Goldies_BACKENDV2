74a8b53dd034928dee1bc1d666813219c8e828c6
Success <abhulimensuccess@gmail.com>
2025-03-03 00:17:39 +0100

    cart endpoints

--- src/models/Cart.model.ts
+++ src/models/Cart.model.ts
@@ -1,28 +1,67 @@
-import { model, Document, Schema } from "mongoose";
+// import { model, Document, Schema } from "mongoose";
 
-interface cartSchemaI extends Document {
+// interface cartSchemaI extends Document {
+//   size: string;
+//   toppings: string;
+//   dateNeeded: string;
+//   details: string;
+//   quantity: number
+// }
+
+// const cartSchema = new Schema<cartSchemaI>({
+//   size: { type: String, require: [true, "Please provide cake size"] },
+//   toppings: {
+//     type: String,
+//     require: [true, "Please provide cake toppings"],
+//   },
+//   dateNeeded: {
+//     type: String,
+//     require: [true, "Please provide when the cake is needed"],
+//   },
+//   details: { type: String, require: [true, "Please provide cake details"] },
+//   quantity: { type: Number, required: [true, "Please provide the quantity for the product"]}
+// }, {
+//   timestamps: true
+// });
+
+// const Cart = model<cartSchemaI>("Cart", cartSchema);
+// export default Cart;
+
+
+import mongoose, { Schema, Document } from "mongoose";
+
+interface ICartProduct {
+  productId: mongoose.Types.ObjectId;
   size: string;
   toppings: string;
+  flavour: string;
   dateNeeded: string;
   details: string;
-  quantity: number
+  quantity: number;
 }
 
-const cartSchema = new Schema<cartSchemaI>({
-  size: { type: String, require: [true, "Please provide cake size"] },
-  toppings: {
-    type: String,
-    require: [true, "Please provide cake toppings"],
-  },
-  dateNeeded: {
-    type: String,
-    require: [true, "Please provide when the cake is needed"],
-  },
-  details: { type: String, require: [true, "Please provide cake details"] },
-  quantity: { type: Number, required: [true, "Please provide the quantity for the product"]}
-}, {
-  timestamps: true
-});
+export interface ICart extends Document {
+  userId: mongoose.Types.ObjectId;
+  products: ICartProduct[];
+}
 
-const Cart = model<cartSchemaI>("Cart", cartSchema);
+const cartSchema = new Schema<ICart>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+        size: { type: String, required: [true, "Please provide cake size"] },
+        toppings: { type: String, required: [true, "Please provide cake toppings"] },
+        flavour: { type: String, required: [true, "Please provide cake flavour"] },
+        dateNeeded: { type: String, required: [true, "Please provide when the cake is needed"] },
+        details: { type: String, required: [true, "Please provide cake details"] },
+        quantity: { type: Number, required: [true, "Please provide the quantity"], min: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model<ICart>("Cart", cartSchema);
 export default Cart;
--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -2,13 +2,20 @@ import { Router } from "express";
 const router = Router();
 
 import { authenticateToken } from "../middleware/verifyJWT";
-import { addToCart, deleteCart } from "../controllers/cartController";
+import { addToCart, clearCart, getCart, removeCartItem, updateCartItem } from "../controllers/cartController";
 
-router.post("/add_to_cart", authenticateToken, addToCart);
-router.delete(
-  "/delete_cart/:cartId",
-  authenticateToken,
-  deleteCart
-);
+router.post("/add", authenticateToken, addToCart);
+router.patch("/update-cart", authenticateToken, updateCartItem);
+router.get("/", authenticateToken, getCart);
+router.delete("/clear", authenticateToken, clearCart);
+router.delete("/remove/:productId", authenticateToken, removeCartItem);
+
+
+
+// router.delete(
+//   "/delete_cart/:cartId",
+//   authenticateToken,
+//   deleteCart
+// );
 
 export default router;
